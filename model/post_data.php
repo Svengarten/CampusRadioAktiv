89ce9c5372c7348cc909fd967e8ada4227b80303
Svengarten <matthies.iwersen123@gmail.com>
2024-10-06 15:34:03 +0200

    neu

--- model/post_data.php
+++ model/post_data.php
@@ -1,48 +1,49 @@
 <?php
 $posts = array(
-    "1" => array("username" => "billieeilish",
+    "1" => array(
+        "username" => "campusradioaktiv", // Ersetze dies mit dem richtigen Benutzernamen
         "userid" => 1,
-        "user_realname" => "Billie Eilish",
-        "user_image" => "billie.jpeg",
-        "post_text" => "come in stream",
-        "post_tags" => array("foryou", "billie", "love"),
-        "post_video_file" => "video.mp4",
+        "user_realname" => "Jannik",
+        "user_image" => "Insta.jpg", // Ersetze dies mit dem richtigen Bildpfad
+        "post_text" => '🎙️ Begleitet Jannik hinter die Kulissen von Campus Radio Aktiv und seinem Podcast Kielafornia. Hier ist ein kleiner Vorgeschmack auf seine nächsten Interviews bei „Janniks Straßenumfrage“ zum Thema feste Beziehung. Diese gibt es in der nächsten Donnerstag Morning Sendung zwischen 8-10 Uhr bei Kiel FM und in der nächsten Podcastfolge! 🙌🏼',
+        "post_tags" => array('#festebeziehungen', '#podcast', '#interviews', '#studentenradio', '#campusradioaktiv'),
+        "post_video_file" => '/assets/videos/src/Yannik.mp4', // Stelle sicher, dass dies korrekt ist
     ),
-    "2" => array("username" => "edsheran",
-        "userid" => 2,
-        "user_realname" => "Ed Sheran",
-        "user_image" => "ed_sheran.jpeg",
-        "post_text" => "check this out",
-        "post_tags" => array("music", "chill", 'guitar'),
-        "post_video_file" => "video",
-
-    ),
-    "3" => array("username" => "edsheran",
-        "userid" => 2,
-        "user_realname" => "Ed Sheran",
-        "user_image" => "ed_sheran.jpeg",
-        "post_text" => "new almbum",
-        "post_tags" => array("new", "peace", "guitar"),
-        "post_video_file" => "video"
-    ),
-    "4" => array("username" => "edsheran",
-        "userid" => 2,
-        "user_realname" => "Ed Sheran",
-        "user_image" => "ed_sheran.jpeg",
-        "post_text" => "Check out billie",
-        "post_tags" => array("billie", "love", "chill"),
-        "post_video_file" => "video"
-    ),
-    "5" => array("username" => "billieeilish",
+    "2" => array(
+        "username" => "campusradioaktiv",
         "userid" => 1,
-        "user_realname" => "Billie Eilish",
-        "user_image" => "billie.jpeg",
-        "post_text" => "at the beach chilling",
-        "post_tags" => array("together", "forever", "love"),
-        "post_video_file" => "video"
+        "user_realname" => "Jannik",
+        "user_image" => "Insta.jpg",
+        "post_text" => 'BEHIND THE SCENES - unsere Schampusradio Aktiv Show 2023 🪩🎊🍾 Was ging bei unserer letzten Show eigentlich hinter den Kulissen ab? Jetzt habt ihr eure Antwort. 🫣🥂 Wir hoffen ihr seid alle gut ins neue Jahr gerutscht. An alle Student:innen: Viel Erfolg bei der Klausurenphase. Ihr packt das!!! ✊🏼',
+        "post_tags" => array('#schampusradioaktiv', '#Radio'),
+        "post_video_file" => '/assets/videos/src/Silvester.mp4', // Stelle sicher, dass dies korrekt ist
     ),
-
-
-
+    "3" => array(
+        "username" => "campusradioaktiv",
+        "userid" => 1,
+        "user_realname" => "Jannik",
+        "user_image" => "Insta.jpg",
+        "post_text" => '🍿 Schaue den ganzen Stream als VOD jetzt auf Twitch!',
+        "post_tags" => array('#oscar', '#oscars', '#oscars2024', '#fachhochschulekiel', '#fhkiel'),
+        "post_video_file" => '/assets/videos/src/Oscar3.mp4', // Stelle sicher, dass dies korrekt ist
+    ),
+    "4" => array(
+        "username" => "campusradioaktiv",
+        "userid" => 1,
+        "user_realname" => "Jannik",
+        "user_image" => "Insta.jpg",
+        "post_text" => '🍿 Schaue den ganzen Stream als VOD jetzt auf Twitch!',
+        "post_tags" => array('#oscar', '#oscars', '#oscars2024', '#fachhochschulekiel', '#fhkiel'),
+        "post_video_file" => '/assets/videos/src/Oscar2.mp4', // Stelle sicher, dass dies korrekt ist
+    ),
+    "5" => array(
+        "username" => "campusradioaktiv",
+        "userid" => 1,
+        "user_realname" => "Jannik",
+        "user_image" => "Insta.jpg",
+        "post_text" => '🍿 Schaue den ganzen Stream als VOD jetzt auf Twitch!',
+        "post_tags" => array('#oscar', '#oscars', '#oscars2024', '#fachhochschulekiel', '#fhkiel'),
+        "post_video_file" => '/assets/videos/src/Oscar1.mp4', // Stelle sicher, dass dies korrekt ist
+    ),
 );
 ?>
--- web/social.php
+++ web/social.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../path/to/post_data.php'; // Stelle sicher, dass der Pfad korrekt ist
 
 // Funktion zum Generieren von Thumbnails
 function generateThumbnail($videoPath, $thumbnailPath) {
@@ -8,77 +9,32 @@ function generateThumbnail($videoPath, $thumbnailPath) {
     return $return_var === 0;
 }
 
-// Beispiel-Daten für Posts
-$posts = [
-    [
-        'nickname' => 'Instagram',
-        'avatar' => '/assets/img/Insta.jpg',
-        'hashtags' => ['#festebeziehungen', '#podcast', '#interviews', '#studentenradio', '#campusradioaktiv'],
-        'video' => '/assets/videos/src/Yannik.mp4',
-        'thumbnail' => '/assets/videos/src/Silvester.mp4',
-        'text' => '🎙️ Begleitet Jannik hinter die Kulissen von Campus Radio Aktiv und seinem Podcast Kielafornia. Hier ist ein kleiner Vorgeschmack auf seine nächsten Interviews bei „Janniks Straßenumfrage“ zum Thema feste Beziehung. Diese gibt es in der nächsten Donnerstag Morning Sendung zwischen 8-10 Uhr bei Kiel FM und in der nächsten Podcastfolge! 🙌🏼',
-        'likes' => 23
-    ],
-    [
-        'nickname' => 'Instagram',
-        'avatar' => '/assets/img/Insta.jpg',
-        'hashtags' => ['#schampusradioaktiv', '#Radio'],
-        'video' => '/assets/videos/src/Silvester.mp4',
-        'thumbnail' => '/assets/videos/thumb/1_event.jpg',
-        'text' => 'BEHIND THE SCENES - unsere Schampusradio Aktiv Show 2023 🪩🎊🍾🪩🎊🍾🪩🎊🍾🪩🎊🍾 Was ging bei unserer letzten Show eigentlich hinter den Kulissen ab? Jetzt habt ihr eure Antwort. 🫣🥂 Wir hoffen ihr seid alle gut ins neue Jahr gerutscht. An alle Student:innen: Viel Erfolg bei der Klausurenphase. Ihr packt das!!! ✊🏼',
-        'likes' => 18
-    ],
-    [
-        'nickname' => 'Instagram',
-        'avatar' => '/assets/img/Insta.jpg',
-        'hashtags' => ['#oscar', '#oscars', '#oscars2024', '#fachhochschulekiel', '#fhkiel'],
-        'video' => '/assets/videos/src/Oscar3.mp4',
-        'thumbnail' => '/assets/videos/thumb/2_live.jpg',
-        'text' => '🍿 Schaue den ganzen Stream als VOD jetzt auf Twitch!',
-        'likes' => 30
-    ],
-    [
-        'nickname' => 'Instagram',
-        'avatar' => '/assets/img/Insta.jpg',
-        'hashtags' => ['#oscar', '#oscars', '#oscars2024', '#fachhochschulekiel', '#fhkiel'],
-        'video' => '/assets/videos/src/Oscar2.mp4',
-        'thumbnail' => '/assets/videos/thumb/3_interview.jpg',
-        'text' => '🍿 Schaue den ganzen Stream als VOD jetzt auf Twitch!',
-        'likes' => 25
-    ],
-    [
-        'nickname' => 'Instagram',
-        'avatar' => '/assets/img/Insta.jpg',
-        'hashtags' => ['#oscar', '#oscars', '#oscars2024', '#fachhochschulekiel', '#fhkiel'],
-        'video' => '/assets/videos/src/Oscar1.mp4',
-        'thumbnail' => '/assets/videos/thumb/4_community.jpg',
-        'text' => '🍿 Schaue den ganzen Stream als VOD jetzt auf Twitch!',
-        'likes' => 40
-    ],
-];
-
 // Beispielhafte Verwendung in social.php
 
 // Wenn der Parameter 'nickname' in der URL vorhanden ist
 if (isset($_GET['nickname'])) {
-        $nickname = htmlspecialchars($_GET['nickname']); // Sichere Ausgabe
-        // Hier kannst du dann die Logik für den Nickname einfügen
-        echo "Profil von " . $nickname;
-    } 
+    $nickname = htmlspecialchars($_GET['nickname']); // Sichere Ausgabe
+    // Hier kannst du dann die Logik für den Nickname einfügen
+    echo "Profil von " . $nickname;
+} 
     
-    // Wenn der Parameter 'tag' in der URL vorhanden ist
-    if (isset($_GET['tag'])) {
-        $tag = htmlspecialchars($_GET['tag']); // Sichere Ausgabe
-        // Hier kannst du dann die Logik für das Hashtag einfügen
-        echo "Beiträge zu " . $tag;
-    } 
+// Wenn der Parameter 'tag' in der URL vorhanden ist
+if (isset($_GET['tag'])) {
+    $tag = htmlspecialchars($_GET['tag']); // Sichere Ausgabe
+    // Hier kannst du dann die Logik für das Hashtag einfügen
+    echo "Beiträge zu " . $tag;
+}
+
 // Generiere Thumbnails für jedes Video (optional)
 foreach ($posts as $post) {
-    if (!file_exists($post['thumbnail'])) { // Wenn das Thumbnail noch nicht existiert
-        generateThumbnail($post['video'], $post['thumbnail']);
+    // Setze den Thumbnail-Pfad basierend auf dem Benutzernamen und der Videonummer
+    $thumbnailPath = '/assets/videos/thumb/' . basename($post['post_video_file'], '.mp4') . '_thumbnail.jpg';
+    if (!file_exists($thumbnailPath)) { // Wenn das Thumbnail noch nicht existiert
+        generateThumbnail($post['post_video_file'], $thumbnailPath);
     }
 }
 
+// Initialisiere Twig
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader);
 
