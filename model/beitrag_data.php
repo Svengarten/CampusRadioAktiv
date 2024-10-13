951aafda18545be08f164259745eaeebf8b97e22
Svengarten <matthies.iwersen123@gmail.com>
2024-10-13 14:04:22 +0200

    Add files via upload

---
+++ model/beitrag_data.php
@@ -0,0 +1,9 @@
+<?php
+$current_post = [
+    'user_realname' => 'Max Mustermann',
+    'user_image' => 'assets/img/Olli.jpg',
+    'post_text' => 'Das ist der Inhalt des aktuellen Beitrags. Hier wird eine spannende Geschichte erzählt.',
+    'post_audio_file' => 'assets/audio/current_post.mp3',
+];
+
+?>
